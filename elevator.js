94a6244e974e1e9d81654b244bc476de639785dd
Alex Conway <alexconway808@gmail.com>
2014-08-16 13:23:04 -1000

    passed all tests

--- elevator.js
+++ elevator.js
@@ -4,15 +4,27 @@
 module.exports = {
   //setting the goto object of function
   goto: function(currentfloor, buttonpressed){
-    var start = currentfloor;
     var convertButtonToInteger = parseInt(buttonpressed);
-    var rideUp = start + convertButtonToInteger;
-    var rideDown = convertButtonToInteger - start;
-    if (currentfloor === 0) {
+    var rideUp = currentfloor + convertButtonToInteger;
+    var rideDown = convertButtonToInteger - currentfloor;
+
+    if (buttonpressed instanceof Object || currentfloor instanceof Object){
+      return 0;
+    }
+    if (typeof currentfloor === 'string' || typeof buttonpressed === 'number'){
+      return 0;
+    }
+    if (currentfloor === 0 && convertButtonToInteger <= 3) {
       return rideUp;
     }
     if (currentfloor <= 3 && currentfloor > 0) {
       return rideDown;
+    }
+    if (convertButtonToInteger >= 4) {
+      return currentfloor;
+    }
+    if (currentfloor >= 4){
+      return convertButtonToInteger;
     }
   }
 };
