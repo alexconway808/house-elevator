79bf902b5bb155a7bdfc1c938ec4a486eeb5a74b
Alex Conway <alexconway808@gmail.com>
2014-08-16 11:18:00 -1000

    passed second round of tests

--- elevator.js
+++ elevator.js
@@ -6,8 +6,13 @@ module.exports = {
   goto: function(currentfloor, buttonpressed){
     var start = currentfloor;
     var convertButtonToInteger = parseInt(buttonpressed);
-    var ride = start + convertButtonToInteger;
-    return ride;
-
+    var rideUp = start + convertButtonToInteger;
+    var rideDown = convertButtonToInteger - start;
+    if (currentfloor === 0) {
+      return rideUp;
+    }
+    if (currentfloor <= 3 && currentfloor > 0) {
+      return rideDown;
+    }
   }
 };
